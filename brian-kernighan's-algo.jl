59662ee0037dec587b69d19a11348e68f26cbf66
A. Alherbawi <abdulraheem.alherbawi@metu.edu.tr>
2026-06-29 22:35:27 +0300

    this algorithm counts the number of set bits in a given binary representation of a positive integer

--- brian-kernighan's-algo.jl
+++ brian-kernighan's-algo.jl
@@ -5,7 +5,7 @@ function setdigitcounter(number) #where number is in base 10 (denary)
     n = number
     count = 0
     while n != 0 # arithemtic logic operation
-        n = n & (n-1) # bitwise logic and arithmetic operation
+        n = n & (n-1) # bitwise logic and arithmetic operation # this is the brian kernighan step
         count += 1
     end
     return count
